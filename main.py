7cc5bd3bd7c9f4d47c0f659247b33b4f1e7db78e
MisterT <lordofcats702@gmail.com>
2021-05-30 22:10:06 +0200

    added main and first marker

---
+++ main.py
@@ -0,0 +1,32 @@
+import cv2
+
+method = cv2.TM_SQDIFF_NORMED
+
+# Read the images from the file
+small_image = cv2.imread('marker2.png')
+large_image = cv2.imread('scan.jpg')
+
+
+
+result = cv2.matchTemplate(small_image, large_image, method)
+
+# We want the minimum squared difference
+mn,_,mnLoc,_ = cv2.minMaxLoc(result)
+
+# Draw the rectangle:
+# Extract the coordinates of our best match
+MPx,MPy = mnLoc
+
+# Step 2: Get the size of the template. This is the same size as the match.
+trows,tcols = small_image.shape[:2]
+
+# Step 3: Draw the rectangle on large_image
+cv2.rectangle(large_image, (MPx,MPy),(MPx+tcols,MPy+trows),(0,0,255),2)
+
+img = cv2.resize(large_image, (800, 1080))  
+
+# Display the original image with the rectangle around the match.
+cv2.imshow('output',img)
+
+# The image is only displayed if we call this
+cv2.waitKey(0)
