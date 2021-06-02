66b3c6a53988a7c22ea5f5b37e195c0cc8289bf6
1MisterT <lordofcats702@gmail.com>
2021-06-02 16:14:21 +0200

    add all images together and save the image

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
 
+res = 1000
+treshhold = 40
+
 img = cv2.imread("scan.jpg")
 height, width = img.shape[:2]
 crop_img = img[0:height, int(width/10):int(width/7)]
@@ -31,26 +34,38 @@ for i in contours:
         box = cv2.boxPoints(rect)
         
         box = np.int0(box)
-        cv2.drawContours(img, [box], 0, (0, 255, 0),5)
-        ares.append([box[0][1].tolist(), box[2][1].tolist()])
+        # cv2.drawContours(img, [box], 0, (0, 255, 0),5)
+        ares.append([box[0][1].tolist()-treshhold, box[2][1].tolist()+treshhold])
 
 impart = []
+n_h = 0
 for a in ares:
-    impart.append(img[a[0]-50:a[1]+50, 0:width])
+    cut = img[a[0]:a[1], :width]
+    impart.append([cut,cut.shape[0], n_h])
+    n_h += cut.shape[0]
     
 
-crop_img = cv2.resize(crop_img, (100,1000))
+# crop_img = cv2.resize(crop_img, (100,1000))
 
-res = 1500
-# ratio = img.shape[0]/img.shape[1]
-# img = cv2.resize(img, (res, int(res*ratio)))
+new_img = np.zeros((n_h, width,3), np.uint8)
 
 # Display the original image with the rectangle around the match.
 for part in impart:
-    indx = impart.index(part)
-    ratio = part.shape[0]/part.shape[1]
-    part = cv2.resize(part, (res, int(res*ratio)))
-    cv2.imshow(f"Part {indx}", part)
+    new_img[part[2]:part[2]+part[1], :part[0].shape[1], :3] = part[0]
+    # print(im.shape==part[0].shape)
+    # indx = impart.index(part)
+ratio = new_img.shape[0]/new_img.shape[1]
+new_img = cv2.resize(new_img, (res, int(res*ratio)))
+    # cv2.imshow(f"Part {indx}", part)
+
+
+cv2.imshow("tse", new_img)
+
+# r = cv2.selectROI(part)
+
+
+
 
 if cv2.waitKey(0) & 0xFF == ord('q'):
     cv2.destroyAllWindows()
+    cv2.imwrite("doc.jpg", new_img)
